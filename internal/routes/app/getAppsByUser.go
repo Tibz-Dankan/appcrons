2c5d6f29b0fe834b8e63b8890d58737b0082ab34
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-02-06 23:41:11 +0300

    feat: add api to get apps by user and by id

--- internal/routes/app/getAppsByUser.go
+++ internal/routes/app/getAppsByUser.go
@@ -1,1 +1,36 @@
-// TODO: to apps by user
+package app
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Tibz-Dankan/keep-active/internal/models"
+	"github.com/gorilla/mux"
+)
+
+func getAppByUser(w http.ResponseWriter, r *http.Request) {
+	app := models.App{}
+	// get userId from query params
+
+	newApp := map[string]interface{}{
+		// "id":              appId,
+		"name":            app.Name,
+		"requestInterval": app.RequestInterval,
+		"updatedAt":       app.UpdatedAt,
+		"createdAt":       app.CreatedAt,
+	}
+
+	response := map[string]interface{}{
+		"status":  "success",
+		"message": "Created successfully",
+		"app":     newApp,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(response)
+}
+
+func GetAppByUserByUser(router *mux.Router) {
+	router.HandleFunc("/api/v1/app/get-by-user", getAppByUser).Methods("GET")
+}
