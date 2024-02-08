2ab4670d9a192c0972f90755cc27c32b0d9153db
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-02-09 00:48:43 +0300

    feat: complete functionality to get single app by its Id

--- internal/routes/app/getApp.go
+++ internal/routes/app/getApp.go
@@ -5,25 +5,27 @@ import (
 	"net/http"
 
 	"github.com/Tibz-Dankan/keep-active/internal/models"
+	"github.com/Tibz-Dankan/keep-active/internal/services"
 	"github.com/gorilla/mux"
 )
 
 func getApp(w http.ResponseWriter, r *http.Request) {
 	app := models.App{}
-	// get appId from params
 
-	newApp := map[string]interface{}{
-		// "id":              appId,
-		"name":            app.Name,
-		"requestInterval": app.RequestInterval,
-		"updatedAt":       app.UpdatedAt,
-		"createdAt":       app.CreatedAt,
+	appId := mux.Vars(r)["appId"]
+
+	app, err := app.FindOne(appId)
+	if err != nil {
+		services.AppError(err.Error(), 500, w)
 	}
 
+	data := map[string]interface{}{
+		"app": app,
+	}
 	response := map[string]interface{}{
 		"status":  "success",
-		"message": "Created successfully",
-		"app":     newApp,
+		"message": "App fetched",
+		"data":    data,
 	}
 
 	w.Header().Set("Content-Type", "application/json")
